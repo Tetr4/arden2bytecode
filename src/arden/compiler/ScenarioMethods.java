dbfabb3193a5f1ac577e6fd6052302541f26b9e6
Mike Klimek <m.klimek@tu-bs.de>
2016-10-22 01:30:55 +0200

    Add more scenario helper methods

--- src/arden/compiler/ScenarioMethods.java
+++ src/arden/compiler/ScenarioMethods.java
@@ -1,5 +1,6 @@
 package arden.compiler;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 import org.junit.Assert;
@@ -7,6 +8,7 @@ import org.junit.Assert;
 import arden.runtime.ArdenTime;
 import arden.runtime.ArdenValue;
 import arden.runtime.MedicalLogicModule;
+import arden.runtime.validation.Call;
 import arden.runtime.validation.ScenarioEngine;
 import arden.runtime.validation.ScenarioExecutionContext;
 import arden.runtime.validation.ScenarioExpressionHelpers;
@@ -15,61 +17,75 @@ import arden.runtime.validation.ScenarioExpressionHelpers;
 final class ScenarioMethods {
 	public static final Method setQuery;
 	public static final Method setInterface;
+	public static final Method getWrittenMessages;
+	public static final Method getReturnedValues;
+	public static final Method getMlmCalls;
+	public static final Method getEventCalls;
 	public static final Method callMlm;
 	public static final Method callEvent;
 	public static final Method setTime;
-	public static final Method getReturnedValues;
-	public static final Method getWrittenMessages;
-	public static final Method assertIsTriggered;
-	public static final Method assertConclude;
-	public static final Method assertMlmCalled;
-	public static final Method assertEventCalled;
+	
+	public static final Field mlmCallDelay;
+	public static final Field mlmCallArgs;
+	
 	public static final Method assertNothingCalled;
 	public static final Method assertNothingReturned;
 	public static final Method assertNothingWritten;
+	public static final Method assertIsTriggered;
+	public static final Method assertConclude;
+	
 	public static final Method assertTrue;
 	public static final Method assertEquals;
 	public static final Method fail;
-	public static final Method assertValidFilterResult;
-	public static final Method assertMessageInList;
-	public static final Method assertSameNumberOfValues;
-	public static final Method isSameNumberOfValues;
 	public static final Method equals;
+	
+	public static final Method assertMessageInList;
+	public static final Method assertValidFilterResult;
+	public static final Method assertSameNumberOfReturnValues;
+	public static final Method isSameNumberOfValues;
 	public static final Method isValidFilterResult;
-	public static final Method moreMessagesAvailable;
+	public static final Method moreItemsAvailable;
+	public static final Method isZeroDelay;
+
 
 	static {
 		try {
 			setQuery = ScenarioExecutionContext.class.getMethod("setQuery", String.class, ArdenValue[].class);
 			setInterface = ScenarioExecutionContext.class.getMethod("setInterface", String.class, ArdenValue[].class);
 			getWrittenMessages = ScenarioExecutionContext.class.getMethod("getWrittenMessages", String.class);
-			getReturnedValues = ScenarioEngine.class.getMethod("getReturnedValues", MedicalLogicModule.class);
-			callMlm = ScenarioEngine.class.getMethod("callMlm", MedicalLogicModule.class, ArdenValue[].class);
+			getMlmCalls = ScenarioExecutionContext.class.getMethod("getMlmCalls", MedicalLogicModule.class);
+			getEventCalls = ScenarioExecutionContext.class.getMethod("getEventCalls", String.class);
+			getReturnedValues = ScenarioEngine.class.getMethod("getReturnedValues");
+			callMlm = ScenarioEngine.class.getMethod("callMlm", ArdenValue[].class);
 			callEvent = ScenarioEngine.class.getMethod("callEvent", String.class, ArdenTime.class, ArdenTime.class);
 			setTime = ScenarioEngine.class.getMethod("setTime", ArdenTime.class);
 			
-			assertMlmCalled = ScenarioExecutionContext.class.getMethod("assertCalled", boolean.class, MedicalLogicModule.class, ArdenValue[].class, ArdenValue.class);
-			assertEventCalled = ScenarioExecutionContext.class.getMethod("assertCalled", boolean.class, String.class, ArdenValue.class);
+			mlmCallDelay = Call.class.getDeclaredField("delay");
+			mlmCallArgs = Call.class.getDeclaredField("args");
+			
 			assertNothingCalled = ScenarioExecutionContext.class.getMethod("assertNothingCalled", boolean.class);
-			assertNothingWritten = ScenarioExecutionContext.class.getMethod("assertNothingWritten", String.class);
-			assertNothingReturned = ScenarioEngine.class.getMethod("assertNothingReturned", boolean.class, MedicalLogicModule.class);
-			assertIsTriggered = ScenarioEngine.class.getMethod("assertIsTriggered", boolean.class, MedicalLogicModule.class);
-			assertConclude = ScenarioEngine.class.getMethod("assertConclude", boolean.class, MedicalLogicModule.class, ArdenValue.class);
+			assertNothingWritten = ScenarioExecutionContext.class.getMethod("assertNothingWritten", boolean.class, String.class);
+			assertNothingReturned = ScenarioEngine.class.getMethod("assertNothingReturned", boolean.class);
+			assertIsTriggered = ScenarioEngine.class.getMethod("assertIsTriggered", boolean.class);
+			assertConclude = ScenarioEngine.class.getMethod("assertConclude", boolean.class, ArdenValue.class);
 			
 			assertTrue = Assert.class.getMethod("assertTrue", String.class, boolean.class);
 			assertEquals = Assert.class.getMethod("assertEquals", String.class, Object.class, Object.class);
 			fail = Assert.class.getMethod("fail", String.class);
 			equals = Object.class.getMethod("equals", Object.class);
 			
-			assertMessageInList = ScenarioExpressionHelpers.class.getMethod("assertMessageInList", ArdenValue.class, ArdenValue[].class);
+			assertMessageInList = ScenarioExpressionHelpers.class.getMethod("assertMessageInList", boolean.class, ArdenValue.class, ArdenValue[].class, String.class);
 			assertValidFilterResult = ScenarioExpressionHelpers.class.getMethod("assertValidFilterResult", ArdenValue.class, ArdenValue.class, int.class);
-			assertSameNumberOfValues = ScenarioExpressionHelpers.class.getMethod("assertSameNumberOfValues", int.class, ArdenValue[].class);
-			isSameNumberOfValues = ScenarioExpressionHelpers.class.getMethod("isSameNumberOfValues", int.class, ArdenValue[].class);
+			assertSameNumberOfReturnValues = ScenarioExpressionHelpers.class.getMethod("assertSameNumberOfReturnValues", int.class, ArdenValue[].class);
+			isSameNumberOfValues = ScenarioExpressionHelpers.class.getMethod("isSameNumberOfValues", int.class, Object[].class);
 			isValidFilterResult = ScenarioExpressionHelpers.class.getMethod("isValidFilterResult", ArdenValue.class);
-			moreMessagesAvailable = ScenarioExpressionHelpers.class.getMethod("moreMessagesAvailable", int.class, ArdenValue[].class);
+			moreItemsAvailable = ScenarioExpressionHelpers.class.getMethod("moreItemsAvailable", int.class, Object[].class);
+			isZeroDelay = ScenarioExpressionHelpers.class.getMethod("isZeroDelay", ArdenValue.class);
 		} catch (SecurityException e) {
 			throw new RuntimeException(e);
 		} catch (NoSuchMethodException e) {
+			throw new RuntimeException(e);
+		} catch (NoSuchFieldException e) {
 			throw new RuntimeException(e);
 		}
 	}
--- src/arden/runtime/validation/ScenarioExpressionHelpers.java
+++ src/arden/runtime/validation/ScenarioExpressionHelpers.java
@@ -1,14 +1,18 @@
 package arden.runtime.validation;
 
+import java.util.Arrays;
+
 import org.junit.Assert;
 
 import arden.runtime.ArdenBoolean;
+import arden.runtime.ArdenDuration;
+import arden.runtime.ArdenTime;
 import arden.runtime.ArdenValue;
 
 public class ScenarioExpressionHelpers {
 
-	public static void assertSameNumberOfValues(int expectedCount, ArdenValue[] actual) {
-		if (actual.length > expectedCount ) {
+	public static void assertSameNumberOfReturnValues(int expectedCount, ArdenValue[] actual) {
+		if (actual.length > expectedCount) {
 			String msg = "Too many values were returned: " + commaSeparatedList(actual);
 			Assert.fail(msg);
 		} else if (actual.length < expectedCount) {
@@ -16,40 +20,47 @@ public class ScenarioExpressionHelpers {
 			Assert.fail(msg);
 		}
 	}
-	
-	public static boolean isSameNumberOfValues(int expectedCount, ArdenValue[] actual) {
+
+	public static boolean isSameNumberOfValues(int expectedCount, Object[] actual) {
 		return expectedCount == actual.length;
 	}
-	
-	public static boolean moreMessagesAvailable(int index, ArdenValue[] messages) {
-		return index < messages.length;
+
+	public static boolean moreItemsAvailable(int index, Object[] items) {
+		return index < items.length;
 	}
 	
 	public static void assertValidFilterResult(ArdenValue filterResult, ArdenValue returnValue, int index) {
 		String msg = "Filter did not match for " + (index + 1) + ". return value: " + returnValue.toString();
 		Assert.assertTrue(msg, filterResult instanceof ArdenBoolean && ArdenBoolean.TRUE.equals(filterResult));
 	}
-	
+
 	public static boolean isValidFilterResult(ArdenValue filterResult) {
 		return ArdenBoolean.TRUE.equals(filterResult);
 	}
 
-	public static void assertMessageInList(ArdenValue message, ArdenValue[] list) {
-		boolean success = false;
-		for (ArdenValue value : list) {
-			if (message.equals(value)) {
-				success = true;
-			}
+	public static void assertMessageInList(boolean not, ArdenValue message, ArdenValue[] list, String destination) {
+		boolean isInList = Arrays.asList(list).contains(message);
+		if (not) {
+			String msg = "Message [" + message + "] is in the list of messages at {" + destination + "}: "
+					+ commaSeparatedList(list);
+			Assert.assertTrue(msg, !isInList);			
+		} else {
+			String msg = "Message [" + message + "] is not in the list of expected messages at {" + destination + "}: "
+					+ commaSeparatedList(list);
+			Assert.assertTrue(msg, isInList);
 		}
-		String msg = "Message [" + message + "] is not in the list of expected messages: " + commaSeparatedList(list);
-		Assert.assertTrue(msg, success);
+
+	}
+	
+	public static boolean isZeroDelay(ArdenValue delay) {
+		return ArdenDuration.seconds(0, ArdenTime.NOPRIMARYTIME).equals(delay);
 	}
 
-	private static String commaSeparatedList(ArdenValue[] values) {
+	public static String commaSeparatedList(Object[] values) {
 		String separator = "";
 		StringBuilder result = new StringBuilder();
 		result.append("[");
-		for (ArdenValue value : values) {
+		for (Object value : values) {
 			result.append(separator);
 			result.append(value.toString());
 			separator = ", ";
